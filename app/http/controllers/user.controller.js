99697735b4afdbb83a108ea2e32933ee779f2773
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-05-01 18:58:34 +0300

    done update user profile

--- app/http/controllers/user.controller.js
+++ app/http/controllers/user.controller.js
@@ -1,3 +1,5 @@
+const { UserModel } = require("../../models/user");
+
 class UserController{
   getProfile(req, res, next){
     try{
@@ -11,8 +13,31 @@ class UserController{
       next(error)
     }
   }
-  editProfile(){
-
+  async editProfile(req, res, next){
+    try{
+      let data = req.body;
+      const userId = req.user._id;
+      let fields = ['fname', 'lname', 'skills']
+      let badValues = ['', ' ', null, undefined, 0, -1, NaN]
+      Object.entries(data).forEach(([key, value]) => {
+        if (!fields.includes(key)) delete data[key]
+        for(let i=0; i<= badValues.length-1; i++){
+          if (badValues[i] == value) delete data[key]
+        }
+      })
+      console.log('data', data)
+      const result = await UserModel.updateOne({_id : userId}, {$set : data})
+      if (result.modifiedCount > 0){
+        return res.status(200).json({
+          status : 200,
+          success : true,
+          message : 'update completed successfully'
+        })
+      }
+      throw {status : 400, message : 'update failed'}
+    } catch (error){
+      next(error)
+    }
   }
   addskills(){
 
--- app/router/user.js
+++ app/router/user.js
@@ -4,6 +4,7 @@ const { checkLogin } = require('../http/middlewares/autoLogin');
 const router = require('express').Router();
 
 router.get('/profile', checkLogin, UserController.getProfile)
+router.post('/profile', checkLogin, UserController.editProfile)
 module.exports = {
   userRoutes : router
 }
