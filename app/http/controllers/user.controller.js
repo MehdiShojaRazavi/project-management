10eca494d940f2673ab7f725ee368841ea4f8d6a
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-05-02 19:03:25 +0300

    done, image user profile convert to link ready for server response

--- app/http/controllers/user.controller.js
+++ app/http/controllers/user.controller.js
@@ -4,6 +4,8 @@ class UserController{
   getProfile(req, res, next){
     try{
       const user = req.user;
+      user.profile_image = req.protocol + '://' + req.get('host') + '/' 
+                          + (user.profile_image.replace(/[\\\\]/g ,'/') )
       return res.status(200).json({
         status : 200,
         success : true,
