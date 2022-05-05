23b814fa1a2ee4b3e7d998258d864359a7aee156
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-05-05 18:15:02 +0300

    Add rout of GetAllProject and test it

--- app/router/project.js
+++ app/router/project.js
@@ -4,6 +4,7 @@ const { expressValidationMapper } = require('../http/middlewares/checkError');
 const { checkLogin } = require('../http/middlewares/autoLogin');
 const { createProjectValidator } = require('../http/validations/project')
 router.post("/create", checkLogin, createProjectValidator(), expressValidationMapper, ProjectController.createProject)
+router.post("/all", checkLogin, expressValidationMapper, ProjectController.getAllProject)
 module.exports = {
   projectRoutes : router
 }
